00725cc53f53828aa587a4ad0a4f87514552fb11
Rex Soriano <awesomerex@gmail.com>
2016-05-19 20:38:56 -0400

    hydrachicken body

--- src/systems/simulation/handle-controller.js
+++ src/systems/simulation/handle-controller.js
@@ -1,17 +1,17 @@
 "use strict";
 
 module.exports = function(ecs, game) { // eslint-disable-line no-unused-vars
-  ecs.addEach(function(entity, elapsed) { // eslint-disable-line no-unused-vars
-  	var controller = game.entities.get(entity, "controller");
-  	var keys = Object.keys(controller);
+	ecs.addEach(function(entity, elapsed) { // eslint-disable-line no-unused-vars
+		var controller = game.entities.get(entity, "controller");
+		var keys = Object.keys(controller);
 
-  	//call script and run it.
-  	for ( var x = 0; x < keys.length; x++){
-  		if ( game.inputs.buttonPressed(keys[x]) ) {
-  			var script = game.require(controller[keys[x]]);
-  			script(entity, game);
-  		}
-  	}
+		//call script and run it.
+		for (var x = 0; x < keys.length; x++) {
+			if (game.inputs.buttonPressed(keys[x])) {
+				var script = game.require(controller[keys[x]]);
+				script(entity, game);
+			}
+		}
 
-  }, "controller");
+	}, "controller");
 };
