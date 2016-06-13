0d4e04c0837fe8cdcf07cefce4b7684dfce759c8
Rex Soriano <awesomerex@gmail.com>
2016-06-12 20:30:47 -0400

    make enemies move

--- src/scripts/spawn-enemy.js
+++ src/scripts/spawn-enemy.js
@@ -3,21 +3,20 @@
 module.exports = function(entity, game) { // eslint-disable-line no-unused-vars
 
 	var spawner = game.entities.find("spawner");
-	var currentLevel = game.entities.get(spawner, "level");
-	var direction = game.entities.get(spawner, "direction");
+	//var currentLevel = game.entities.get(spawner, "level");
+	//var direction = game.entities.get(spawner, "direction");
 	var spawnerPosition = game.entities.get(spawner, "position");
 
-console.log(spawner, spawnerPosition);
+	console.log(spawner, spawnerPosition);
 
-	var level = ["beetle", "rat", "sheep", "pony", "elephant"];
-	var colors = ["red", "blue"];
+	//var level = ["beetle", "rat", "sheep", "pony", "elephant"];
+	//var colors = ["red", "blue"];
 
 	//determine which prefab we're generating
 
 	var critter = game.instantiatePrefab("beetle-blue");
 
 	var critterPos = game.entities.get(critter, "position");
-	var critterImage = game.entities.get(critter, "image");
 
 	critterPos.x = spawnerPosition.x;
 	critterPos.y = spawnerPosition.y;
--- src/systems/simulation/move-enemies.js
+++ src/systems/simulation/move-enemies.js
@@ -2,6 +2,21 @@
 
 module.exports = function(ecs, game) { // eslint-disable-line no-unused-vars
 	ecs.addEach(function(entity, elapsed) { // eslint-disable-line no-unused-vars
-		
+		var position = game.entities.get(entity, "position");
+		var direction = game.entities.get(entity, "direction");
+
+		//check if enemy is off of the screen
+		if (position.x < -20 || position.x > 1156) {
+			game.entities.destroy(entity);
+		}
+
+		if (direction === "right") {
+			position.x += 2;
+		}
+
+		if (direction === "left") {
+			position.x -= 2;
+		}
+
 	}, "enemy");
 };
